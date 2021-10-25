e09b3655f86dd0f0448661039591c91a2864a8f9
Lukasz Gajowy <lukasz.gajowy@adverity.com>
2021-10-25 14:26:34 +0200

    Add initial cats-effect project setup. Read the directory path from CL args

--- build.sbt
+++ build.sbt
@@ -1,3 +1,8 @@
 name := "file-search"
 
 scalaVersion := "2.13.6"
+
+libraryDependencies ++= Seq(
+  "org.typelevel" %% "cats-effect" % "3.2.9",
+  "org.typelevel" %% "cats-core" % "2.6.1",
+)
--- src/main/scala/com/lgajowy/Main.scala
+++ src/main/scala/com/lgajowy/Main.scala
@@ -1,5 +1,16 @@
 package com.lgajowy
 
-object Main {
+import cats.effect._
 
+object Main extends IOApp {
+
+  private case class DirectoryPath(path: String)
+
+  override def run(args: List[String]): IO[ExitCode] =
+    for {
+      _ <- if (args.length < 1) IO.raiseError(new IllegalArgumentException("Please enter the directory path"))
+      else IO.unit
+      path = DirectoryPath(args.head)
+      _ <- IO.println(path)
+    } yield ExitCode.Success
 }
